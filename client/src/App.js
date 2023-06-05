41046188cdf254377a22f5bedba0ea53c9ef29b1
BastienVdp <bastienpp@hotmail.com>
2023-06-05 21:38:38 +0200

    add controller view

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,17 @@ import HomePage from "./pages/HomePage"
 import GamePage from "./pages/GamePage"
 import ConnectPage from "./pages/ConnectPage"
 import {AppProvider} from "./contexts/AppContext";
+import Controller from "./components/Controller";
 
-export const socket = io("http://localhost:3001")
+export const socket = io("http://192.168.1.9:3001")
 
 function App() {
   return <>
   <AppProvider>
     <Routes>
         <Route path="/" exact element={<HomePage socket={socket}/>} />
-        <Route path="/room/:roomId/" element={<GamePage socket={socket}/>} /> {/*:room/:player*/}
+        <Route path="/controller/:roomId/:userId" element={<Controller socket={socket} />} />
+        <Route path="/game/:roomId" element={<GamePage socket={socket}/>} />
         <Route path="/connect" element={<ConnectPage socket={socket}/>} />
     </Routes>
   </AppProvider>

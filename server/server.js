375231b5604ae09bc321c81a95161c9b78b4bf51
BastienVdp <bastienpp@hotmail.com>
2023-06-05 21:42:24 +0200

    add cors origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: ["http://localhost:3000", "http://192.168.1.9:3000"],
 		methods: ["GET", "POST"],
 	}
 });
@@ -25,10 +25,11 @@ const handleRoom = require("./utils/rooms");
 const rooms = [];
 
 io.on("connect", (socket) => {
-	
+	console.log("user connected : ", socket.id)
 	handleRoom(socket, io, rooms)
+
 	socket.on("disconnect", () => {
-		console.log("user disconnected");
+		console.log("user disconnected", socket.id);
 	});
 });
 
